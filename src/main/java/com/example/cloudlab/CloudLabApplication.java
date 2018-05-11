9e763906aaa1e2a4fdb36966f361b67f059196c1
VicSz <vicsz>
2018-05-11 12:27:26 -0400

    Initial commit

---
+++ src/main/java/com/example/cloudlab/CloudLabApplication.java
@@ -0,0 +1,12 @@
+package com.example.cloudlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudLabApplication.class, args);
+	}
+}
